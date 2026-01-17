f2bee99e122f8466c34f3373a5bb3c961333d604
Aniket Thorat <thorataniket088@gmail.com>
2026-01-17 19:37:08 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>Travel Journal</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import globe from '../images/globe.png'
+
+export default function Header(){
+    return (
+        <>
+        <Header>
+            <img src={globe} alt='globe-image'/>
+        </Header>
+        </>
+    )
+}
